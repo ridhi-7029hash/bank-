7a5e47e589cd66560bc80546a258e079d85aabc5
ridhi-7029hash <82357006+ridhi-7029hash@users.noreply.github.com>
2021-08-12 12:46:36 +0530

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Basic Banking System Using PHP and MySQL
+Installation Steps(Configuration)
+1. Download and Unzip the file on your local system.
+2. Put this file inside xampp/htdocs/ .
+3. Database Configuration
+Open phpmyadmin
+Create Database spark_bank.
+Import database spark_bank.sql
+Open Your browser put inside browser “http://localhost/project/”
---
+++ navbar.php
@@ -0,0 +1,23 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-dark">
+      <h3> <a class="navbar-brand" href="index.php" style="color : white"><b>RIANA BANK</b></a> </h3>
+      <img src="images/download.jpg" width="50" height="70" class="img-fluid pt-2">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+               <h3> <a class="nav-link" href="index.php" style="color : white"><b>Home</b></a></h3>
+              </li>
+              <li class="nav-item">
+              <h3> <a class="nav-link" href="transfermoney.php" style="color : white"><b>View Customers</b></a></h3>
+              </li>
+              <li class="nav-item">
+              <h3> <a class="nav-link" href="transfermoney.php" style="color : white"><b>Transfer Money</b></a></h3>
+              </li>
+              <li class="nav-item">
+              <h3> <a class="nav-link" href="transactionhistory.php" style="color : white"><b>View Transaction</b></a></h3>
+              </li>
+          </div>
+       </nav>
